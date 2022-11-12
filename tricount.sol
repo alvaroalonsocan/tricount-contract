0e6cdb00c85b80da0919dbe645e14f39db537f4d
alvaroac99 <alvaroalc99@gmail.com>
2022-11-12 09:32:28 +0100

    changes

--- tricount.sol
+++ tricount.sol
@@ -4,40 +4,56 @@ pragma solidity ^0.8.16;
 
 contract Auction {
 
-    mapping(address => int) public balances;
-    address[] addresses;
-    event Messages(string _message);
-    event insertedAdresses(address);
-
-    constructor(address[] memory addresses_) {
-        addresses = addresses_;
-        for (uint i = 0; i < addresses_.length; i++) {
-            balances[addresses_[i]] = 0;
-        }
-        emit Messages("Created...");
+    struct Balance {
+        int amount;
+        bool exists;
     }
 
+    mapping(address => Balance) public balances;
+    address[] adresses;
+    event Messages(string _message);
+    event insertedAdresses(address);
+    event insertedAdressesBalances(address,int);
+
+    constructor(address[] memory adresses_) {
+        adresses = adresses_;
+        for (uint i = 0; i < adresses_.length; i++) {
+            balances[adresses_[i]] = Balance(0, true);
+        }
+    }
 
     function addAmount(int amount) public {
-        balances[msg.sender] = balances[msg.sender] + amount;
-        int amountToDiscount = amount / (int(addresses.length) - 1);
-        for (uint i = 0; i < addresses.length; i++) {
-            emit Messages("Iterated");
-            if (addresses[i] != msg.sender) {
-                balances[addresses[i]] = balances[addresses[i]] - amountToDiscount;
+        require(adressExistsInBalance(), "Address does no exists in balances");
+        addAmountToAccount(msg.sender, amount);
+        int amountToDiscount = amount / (int(adresses.length) - 1);
+        for (uint i = 0; i < adresses.length; i++) {
+            if (adresses[i] != msg.sender) {
+                discountAmountToAccount(adresses[i], amountToDiscount);
             }
         }
     }
 
+    function adressExistsInBalance() public view returns(bool){
+        return balances[msg.sender].exists;
+    }
+
+    function addAmountToAccount(address expectedAddress, int amount) private {
+        balances[expectedAddress].amount = balances[expectedAddress].amount + amount;
+    }
+
+    function discountAmountToAccount(address expectedAddress, int amount) private {
+        balances[expectedAddress].amount = balances[expectedAddress].amount - amount;
+    } 
+
     function getAdresses() public {
-        for (uint i = 0; i < addresses.length; i++) {
-            emit insertedAdresses(addresses[i]);
+        for (uint i = 0; i < adresses.length; i++) {
+            emit insertedAdresses(adresses[i]);
         }
     }
 
     function getAdressesBalances() public {
-        for (uint i = 0; i < addresses.length; i++) {
-            emit insertedAdresses("Adress",addresses[i]);
+        for (uint i = 0; i < adresses.length; i++) {
+            emit insertedAdressesBalances(adresses[i],balances[adresses[i]].amount);
         }
     }
     
