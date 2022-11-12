4720739016d77cbb435a48c0716d2311212ea431
renatovt11 <renatovt11@gmail.com>
2022-11-12 12:14:42 +0100

    fix defined balances method

--- tricount.sol
+++ tricount.sol
@@ -64,7 +64,7 @@ contract Auction {
 
     function discountToDefinedBalances(BalanceToDiscount[] memory balancesToDiscount) private {
         for (uint i = 0; i < balancesToDiscount.length; i++) {
-            if (adresses[i] != msg.sender) {
+            if (balancesToDiscount[i].addressToDiscount != msg.sender) {
                 discountAmountToAccount(balancesToDiscount[i].addressToDiscount, balancesToDiscount[i].amountToDiscount);
             }
         }
@@ -96,4 +96,4 @@ contract Auction {
 }
 
 //["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
-//50,[["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",10],["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",40]]
+//20,[["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",5],["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",15]]
